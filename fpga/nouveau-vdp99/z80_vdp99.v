eae18b6c249b13608652148cf1819b1b53474cb8
Denno Wiggle <denno@wigglethemouse.com>
2025-02-06 09:12:37 -0800

    Modify WTM_CDC_MOD to match original as it's the same code in a more readable

--- fpga/nouveau-vdp99/z80_vdp99.v
+++ fpga/nouveau-vdp99/z80_vdp99.v
@@ -52,10 +52,25 @@ module z80_vdp99 (
         .reset(reset), .clk1(phi), .clk2(pxclk), .in(cpu_wr_tick), .out(vdp_wr_tick)
     );
 
-    always@(posedge cpu_wr_tick) begin
+    `ifdef SHOULD_NOT_NEED_THIS
+        // cpu_wr_tick ALWAYS raises with falling edge T2.  CPU A and D busses are stable by then
+        always @(posedge cpu_wr_tick) begin
             vdp_mode <= cpu_mode;
             vdp_din  <= cpu_din;
-    end
+        end
+    `else
+        // D (transparent) latches used to ensure that the setup and hold times on 
+        // vdp_mode and vdp_din are long enough for vdp_wr_tick.
+        // nextpnr does not like this
+        // get around it's loop failure with:
+        //      NEXTPNR_OPT+=--ignore-loops 
+        always @(*) begin
+            if ( cpu_wr_tick ) begin
+                vdp_mode <= cpu_mode;
+                vdp_din  <= cpu_din;
+            end
+        end
+    `endif
 `else
 
     //Sync cpu_wr_tick, cpu_din, and cpu_mode into the pxclk domain 
